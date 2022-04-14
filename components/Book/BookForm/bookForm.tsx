750ba346efbd72004772167b5e2629c206c6b48e
Aaron Saban <aaron.saban@mercadolibre.com>
2022-04-14 19:53:21 -0300

    feat: add booking form

--- components/Book/BookForm/bookForm.tsx
+++ components/Book/BookForm/bookForm.tsx
@@ -1,5 +1,33 @@
-import { Box } from "@chakra-ui/layout";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  reservableId: string;
+  userId: string;
+  startDate: string;
+  endDate: string;
+  pricing: string;
+  name: string;
+};
 
 export const BookForm = () => {
-  return <Box> Hola mundo </Box>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
 };
